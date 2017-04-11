02007fcf14a640acd291b2774617220573c6ecba
Yegor Timoshenko <yegortimoshenko@gmail.com>
2017-04-11 14:33:48 +0000

    BREAKING: Implement listeners, decomplect tables and fields/view

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject com.yegortimoshenko/swing "20170407.003110"
+(defproject com.yegortimoshenko/swing "20170410.224932"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.miglayout/miglayout "3.7.4"]]
-  :plugins [[lein-stamp "20170312.223701"]])
+  :plugins [[lein-stamp "20170312.223701"]]
+  :repl-options {:init-ns yegortimoshenko.swing})
--- src/yegortimoshenko/swing.clj
+++ src/yegortimoshenko/swing.clj
@@ -1,58 +1,33 @@
 (ns yegortimoshenko.swing
   (:refer-clojure :exclude [list])
+  (:require [clojure.set :as set]
+            [clojure.string :as str])
   (:import [clojure.lang IAtom IDeref IMeta Seqable]
            [net.miginfocom.layout AC CC LC]
-           net.miginfocom.swing.MigLayout
+           [net.miginfocom.swing MigLayout]
            [java.awt Color Component Dimension Font Insets]
-           java.awt.event.ActionListener
-           java.util.List
+           [java.awt.event ActionListener FocusListener ItemListener
+            WindowListener WindowFocusListener WindowStateListener]
+           [java.util List]
            [javax.swing JButton JCheckBox JComboBox JComponent JFileChooser JFrame JLabel
-            JList JPanel JPasswordField JProgressBar JScrollPane JSlider JTable
-            JTextArea JTextField DefaultComboBoxModel DefaultListModel
+            JList JPanel JPasswordField JProgressBar JScrollPane JSlider JTable JTextArea
+            JTextField DefaultComboBoxModel DefaultListModel DefaultListCellRenderer
             SwingConstants SwingUtilities]
-           javax.swing.table.DefaultTableModel
-           javax.swing.text.JTextComponent))
-
-(defn panel
-  ([components] (panel {} components))
-  ([{:keys [layout column row]} components]
-   (let [p (JPanel.)]
-     (.setLayout p (MigLayout. (str layout) (str column) (str row)))
-     (doseq [c components]
-       (let [[c spec] (if (sequential? c) c [c])]
-         (.add p ^Component c ^Object (str spec)))) p)))
-
-(defmacro invoke [& body]
-  `(let [a# (atom nil)]
-     (SwingUtilities/invokeAndWait #(reset! a# (do ~@body)))
-     (deref a#)))
-
-(defmacro component [proto args g & [s]]
-  (let [this (with-meta 'this {:tag proto})]
-    `(proxy [~proto ~@(if g `[IDeref]) ~@(if s `[IAtom])] [~@args]
-       ~@(if g `[(deref [] (invoke (~g ~this)))])
-       ~@(if s `[(compareAndSet [old# new#] (invoke (let [same?# (= old# (~g ~this))]
-                                                      (if same?# (~s ~this new#)) same?#)))
-                 (reset [new#] (invoke (~s ~this new#) (~g ~this)))
-                 (swap [f# & args#] (loop [] (let [old# (deref ~this)
-                                                   new# (apply f# old# args#)]
-                                               (if-let [res# (invoke (when (= old# (~g ~this))
-                                                                       (~s ~this new#)
-                                                                       (~g ~this)))] res# (recur)))))]))))
+           [javax.swing.event AncestorListener ChangeListener]
+           [javax.swing.table DefaultTableModel DefaultTableCellRenderer TableCellEditor]
+           [javax.swing.text JTextComponent]))
 
 (defprotocol IColor
   (to-color ^Color [this]))
 
-(extend Color
-  IColor
-  {:to-color (fn [this] this)})
-
-(extend List
-  IColor
-  {:to-color (fn [[r g b a]]
-               (if a
-                 (Color. r g b a)
-                 (Color. r g b)))})
+(extend-protocol IColor
+  Color
+  (to-color [this] this)
+  List
+  (to-color [[r g b a]]
+    (if a
+      (Color. r g b a)
+      (Color. r g b))))
 
 (defn to-dimension
   ([w h]
@@ -64,35 +39,123 @@
 (defprotocol IFont
   (to-font [this]))
 
-(extend Font
-  IFont
-  {:to-font (fn [this] this)})
+(extend-protocol IFont
+  Font
+  (to-font [this] this))
 
 (defmacro ^:private sizer [m]
   `(fn [^JComponent c# ^Dimension d#] (~m c# d#)))
 
-(defn- style
-  [^Component c
-   {:keys [width height
-           pref-width pref-height
-           min-width min-height
-           max-width max-height] :as opts}]
-  (doseq [[f w h] [[(sizer .setSize) width height]
-                   [(sizer .setPreferredSize) pref-width pref-height]
-                   [(sizer .setMinimumSize) min-width min-height]
-                   [(sizer .setMaximumSize) max-width max-height]]]
-    (if (or w h)
-      (f c (to-dimension c w h))))
+(def ^:private sizes
+  [[(sizer .setSize) :width :height]
+   [(sizer .setMinimumSize) :min-width :min-height]
+   [(sizer .setMaximumSize) :max-width :max-height]])
+
+(defn- set-sizes! [^Component c opts]
+  (doseq [[f kw kh] sizes]
+    (let [w (opts kw)
+          h (opts kh)]
+      (if (or w h) (f c (to-dimension c w h))))))
+
+(def ^:private nop (fn [& _]))
+
+(defmacro ^:private listener [add i ms]
+  (let [handlers (gensym)
+        ks (mapv (fn [m] (-> m name (str/replace #"[A-Z]" #(str \- (str/lower-case %))) keyword)) ms)
+        ks->ms (zipmap ks ms)]
+    `(fn [c# handlers#]
+       (let [~handlers (set/rename-keys (select-keys handlers# ~ks) (quote ~ks->ms))]
+         (when-not (empty? ~handlers)
+           (~add c# (reify ~i ~@(for [m ms] `(~m [_# e#] ((get ~handlers (quote ~m) ~nop) e#))))))))))
+
+(defmacro ^:private listeners [& body]
+  (cons 'juxt (for [form body] (cons 'listener form))))
+
+(def ^:private listen
+  (listeners
+   [.addActionListener ActionListener [actionPerformed]]
+   [.addAncestorListener AncestorListener [ancestorAdded ancestorMoved ancestorRemoved]]
+   [.addChangeListener ChangeListener [stateChanged]]
+   [.addFocusListener FocusListener [focusGained focusLost]]
+   [.addItemListener ItemListener [itemStateChanged]]
+   [.addWindowListener WindowListener
+    [windowActivated windowDeactivated windowClosed windowClosing windowOpened]]
+   [.addWindowFocusListener WindowFocusListener [windowGainedFocus windowLostFocus]]
+   [.addWindowStateListener WindowStateListener [windowStateChanged]]))
+
+(def ^:private alignment
+  {:center SwingConstants/CENTER
+   :left SwingConstants/LEFT
+   :right SwingConstants/RIGHT})
+
+(defn- style! [^Component c opts]
+  (set-sizes! c opts)
   (doseq [[k v] opts]
     (case k
+      :align (.setHorizontalAlignment c (v alignment))
       :background (.setBackground c (to-color v))
       :foreground (.setForeground c (to-color v))
+      :default (reset! ^IAtom c v)      
       :enabled? (.setEnabled c ^Boolean v)
       :font (.setFont c (to-font v))
+      :listeners (listen c v)
+      :name (.setName c ^String v)
       :opaque? (.setOpaque ^JComponent c ^Boolean v)
       :tooltip (.setToolTipText ^JComponent c ^String v)
       :visible? (.setVisible c ^Boolean v)
       :default)))
+
+(def ^:dynamic *frame-visible?* true)
+
+(defn- frame-defaults [params]
+  (let [close #(-> % .getSource .dispose)]
+    (->> (merge {:visible? *frame-visible?*} params)
+         (merge-with merge {:listeners {:window-closing close}}))))
+
+(defn frame
+  ([c] (frame {} c))
+  ([{:keys [resizable? title] :as opts} c]
+   (doto (JFrame. ^String title)
+     (.add ^Component c)
+     (.pack)
+     (.setDefaultCloseOperation JFrame/DO_NOTHING_ON_CLOSE)
+     (.setResizable (boolean resizable?))
+     (style! (frame-defaults opts)))))
+
+(defn scrollable [c]
+  (JScrollPane. c))
+
+(defmacro invoke [& body]
+  `(if (SwingUtilities/isEventDispatchThread)
+     (do ~@body)
+     (let [a# (atom nil)]
+       (SwingUtilities/invokeAndWait #(reset! a# (do ~@body)))
+       (deref a#))))
+
+(defmacro component [proto args g s]
+  (let [this (with-meta 'this {:tag proto})]
+    `(proxy [~proto IDeref IAtom] [~@args]
+       (deref [] (invoke (~g ~this)))
+       (compareAndSet [old# new#]
+         (invoke (let [same?# (= old# (~g ~this))]
+                   (if same?# (~s ~this new#)) same?#)))
+       (reset [new#]
+         (invoke (~s ~this new#) (~g ~this)))
+       (swap [f# & args#]
+         (loop [] (let [old# (deref ~this)
+                        new# (apply f# old# args#)]
+                    (if-let [res# (invoke (when (= old# (~g ~this))
+                                            (~s ~this new#)
+                                            (~g ~this)))] res# (recur))))))))
+
+(defn panel
+  ([cs] (panel {} cs))
+  ([{:keys [layout column row] :as params} cs]
+   (let [p (JPanel. (MigLayout. (str layout) (str column) (str row)))]
+     (doseq [c cs]
+       (let [[c spec] (if (sequential? c) c [c])]
+         (.add p ^Component c ^Object (str spec))))
+     (doto p (style! params)))))
 
 (defn file-chooser
   ([] (file-chooser {}))
@@ -103,102 +166,118 @@
                          (if save?
                            (.showSaveDialog this parent)
                            (.showOpenDialog this parent)))
-                  (.getSelectedFile this))))))
+                  (.getSelectedFile this)))
+              .setSelectedFile)))
 
-(defn frame
-  ([c] (frame {} c))
-  ([{:keys [resizable? submit title visible?] :or {} :as opts} c]
-   (let [f (JFrame. ^String title)]
-     (if resizable? (.setResizable f resizable?))
-     (if submit (-> f .getRootPane (.setDefaultButton submit)))
-     (if c (.add f ^Component c))
-     (doto f .pack (style (conj opts (when (nil? visible?) [:visible? true])))))))
-
-(defn scrollable [c]
-  (JScrollPane. c))
-
-(defn- from-model [^DefaultComboBoxModel m]
+(defn ^:private from-model [^DefaultComboBoxModel m]
   (vec (for [i (range (.getSize m))] (.getElementAt m i))))
 
-(defn- to-model ^DefaultComboBoxModel [xs]
+(defn ^:private to-model ^DefaultComboBoxModel [xs]
   (let [m (DefaultComboBoxModel.)]
     (doseq [x xs] (.addElement m x)) m))
 
 (defn combo-box
   ([] (combo-box []))
   ([xs] (combo-box {} xs))
-  ([opts xs]
-   (let [c (component JComboBox [(to-model xs)]
-                      (fn [^JComboBox this] (from-model (.getModel this)))
-                      (fn [^JComboBox this ys] (.setModel this (to-model ys))))]
-     (doto c (style opts)))))
+  ([params xs]
+   (doto
+     (component JComboBox [(to-array xs)] .getSelectedItem .setSelectedItem)
+     (style! params))))
+
+(defprotocol IRender
+  (to-component [this ctx]))
+
+(extend-protocol IRender
+  Component
+  (to-component [this _] this))
+
+(defn ^:private positions [pred coll]
+  (for [[idx elt] (map-indexed vector coll) :when (pred elt)] idx))
 
 (defn list
   ([] (list []))
   ([xs] (list {} xs))
-  ([opts xs]
-   (let [c (component JList [(to-model xs)]
-                      (fn [^JList this] (from-model (.getModel this)))
-                      (fn [^JList this ys] (.setModel this (to-model ys))))]
-     (doto c (style opts)))))
+  ([params xs]
+   (doto (component JList [^DefaultListModel (to-model xs)]
+                    (fn [^JList this]
+                      (set (.getSelectedValues this)))
+                    (fn [^JList this ys]
+                      (.setSelectedIndices this (int-array (positions (set ys) (from-model (.getModel this)))))))
+     (.setCellRenderer
+      (proxy [DefaultListCellRenderer] []
+        (getListCellRendererComponent [parent v row selected? focus?]
+          (if (satisfies? IRender v)
+            (to-component v {:parent parent :row row :selected? selected? :focus? focus?})
+            (proxy-super getListCellRendererComponent parent v row selected? focus?)))))
+     (style! params))))
 
 (defn- from-table-model [^DefaultTableModel model]
-  (let [columns (for [i (range (.getColumnCount model))]
-                  (.getColumnName model i))]
-    (into [(vec columns)] (.getDataVector model))))
+  (mapv vec (.getDataVector model)))
 
-(defn- to-table-model [xs]
-  (DefaultTableModel. (to-array-2d (rest xs)) (to-array (first xs))))
+(defn- to-table-model ^DefaultTableModel [rows columns]
+  (DefaultTableModel. (to-array-2d rows) (to-array columns)))
 
 (defn table
-  ([] (table []))
-  ([xs] (table {} xs))
-  ([{:keys [sort?] :as opts} xs]
-   (let [c (component JTable [^DefaultTableModel (to-table-model xs)]
-                      (fn [^JTable this] (from-table-model (.getModel this)))
-                      (fn [^JTable this ys] (.setModel this (to-table-model ys))))]
-     (if sort? (.setAutoCreateRowSorter c true))
-     (doto c (style opts)))))
-
-(def ^:private alignment
-  {:center SwingConstants/CENTER
-   :left SwingConstants/LEFT
-   :right SwingConstants/RIGHT})
+  ([rows] (table rows (repeat (count (first rows)) (str))))
+  ([rows columns] (table {} rows columns))
+  ([{:keys [editable?] :as params} rows columns]
+   (let [c (component JTable [(to-table-model rows columns)]
+                      (fn [^JTable this]
+                        (let [m (from-table-model (.getModel this))]
+                          (set (map (partial nth m) (.getSelectedRows this)))))
+                      (fn [^JTable this ys]
+                        (.clearSelection this)
+                        (doseq [idx (positions (set ys) (from-table-model (.getModel this)))]
+                          (.addRowSelectionInterval this idx idx))))]
+     (if (not editable?) (.setDefaultEditor c Object nil))
+     (doto c
+       (.setDefaultRenderer
+        Object
+        (proxy [DefaultTableCellRenderer] []
+          (getTableCellRendererComponent [parent v selected? focus? row column]
+            (if (satisfies? IRender v)
+              (to-component v {:parent parent :selected? selected? :focus? focus? :row row :column column})
+              (proxy-super getTableCellRendererComponent parent v selected? focus? row column)))))
+       (style! params)))))
 
 (defn button
   ([] (button ""))
   ([s] (button {} s))
-  ([{:keys [align] :as opts} s]
-   (let [c (component JButton [^String s] .getText .setText)]
-     (if align
-       (.setHorizontalAlignment ^JButton c (align alignment)))
-     (doto c (style opts)))))
+  ([params s]
+   (doto
+     (JButton. ^String s)
+     (style! params))))
 
 (defn checkbox
   ([] (checkbox ""))
-  ([s] (checkbox s false))
-  ([s b] (checkbox {} s b))
-  ([opts s b]
-   (doto (component JCheckBox [^String s ^Boolean b] .isSelected .setSelected) (style opts))))
+  ([s] (checkbox {} s))
+  ([params s]
+   (doto
+     (component JCheckBox [^String s] .isSelected .setSelected)
+     (style! params))))
 
 (defn label
   ([] (label ""))
   ([s] (label {} s))
-  ([{:keys [align] :as opts} s]
-   (doto (component JLabel [^String s ^int ((or align :left) alignment)] .getText .setText) (style opts))))
+  ([params s]
+   (doto
+     (JLabel. ^String s)
+     (style! params))))
 
 (defn progress-bar
   ([] (progress-bar 100))
   ([end] (progress-bar 0 end))
   ([start end] (progress-bar {} start end))
-  ([opts start end] (doto (component JProgressBar [start end] .getValue .setValue) (style opts))))
+  ([params start end]
+   (doto
+     (component JProgressBar [start end] .getValue .setValue)
+     (style! params))))
 
 (defn slider
   ([end] (slider 0 end))
-  ([start end] (slider start end (quot (- end start) 2)))
-  ([start end val] (slider {} start end val))
-  ([{:keys [minor major] :as opts} start end val]
-   (let [c (component JSlider [start end val] .getValue .setValue)]
+  ([start end] (slider {} start end))
+  ([{:keys [minor major] :as params} start end]
+   (let [c (component JSlider [start end] .getValue .setValue)]
      (when (or major minor)
        (.setPaintTicks c true)
        (if major
@@ -210,19 +289,26 @@
          (doto c
            (.setMinorTickSpacing minor)
            (.setSnapToTicks true))))
-     (doto c (style opts)))))
+     (doto c (style! params)))))
 
 (defn password-field
-  ([] (password-field ""))
-  ([s] (password-field {} s))
-  ([opts s] (doto (component JPasswordField [^String s] .getPassword .setText) (style opts))))
+  ([] (password-field {}))
+  ([params]
+   (doto (component JPasswordField []
+                    .getPassword
+                    (fn [^JPasswordField this s] (.setText this (apply str s))))
+     (style! params))))
 
 (defn text-area
-  ([] (text-area ""))
-  ([s] (text-area {} s))
-  ([opts s] (doto (component JTextArea [^String s] .getText .setText) (style opts))))
+  ([] (text-area {}))
+  ([params]
+   (doto
+     (component JTextArea [] .getText .setText)
+     (style! params))))
 
 (defn text-field
-  ([] (text-field ""))
-  ([s] (text-field {} s))
-  ([opts s] (doto (component JTextField [^String s] .getText .setText) (style opts))))
+  ([] (text-field {}))
+  ([params]
+   (doto
+     (component JTextField [] .getText .setText)
+     (style! params))))
