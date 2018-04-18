937b7a5bbc26b0b387f34c36387090dda5c59eef
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-04-18 17:05:14 +0000

    Use Clojure tools

--- project.clj
+++
@@ -1,5 +0,0 @@
-(defproject com.yegortimoshenko/ui.swing "20170420.174523"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.miglayout/miglayout "3.7.4"]]
-  :plugins [[lein-stamp "20170312.223701"]]
-  :repl-options {:init-ns yegortimoshenko.ui.swing})
